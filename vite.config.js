be46bb51590193da8ef737ef85f7b81d1f60bd3e
Mohini Upreti <upretimohini@gmail.com>
2024-02-01 22:20:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TV_SHOWS_REACT_APP.github.io",
+  base: "/TV_SHOWS_REACT_APP.github.io/",
   plugins: [react()],
 })
